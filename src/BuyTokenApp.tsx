b1683dc530cb5ab67f004119eab4e01f3b26a704
gb-smoke <gelegbalsan@smoke.money>
2024-12-03 19:47:19 +0800

    added send transaction

--- src/BuyTokenApp.tsx
+++ src/BuyTokenApp.tsx
@@ -12,7 +12,7 @@ import ConnectWallet from "./components/ConnectWallet";
 import logo from "../public/logo4.png";
 
 function BuyTokenApp() {
-  const { fetchRequest, isBorrowToken } = useApi()
+  const { fetchRequest, isBorrowToken } = useApi();
   const { wallets } = useWallets();
   const { sendTransactionAsync } = useSendTransaction();
 
@@ -52,7 +52,7 @@ function BuyTokenApp() {
         setChains(_res);
         setSelectedChain(_res[0]);
       }
-    } catch (error) { }
+    } catch (error) {}
   }
 
   /**
@@ -73,65 +73,78 @@ function BuyTokenApp() {
    * Initiates the process to buy a token by interacting with various APIs and sending a transaction.
    *
    * @param {TokenType} token - The token object containing details such as chainId and address.
-   * 
+   *
    * The function performs the following steps:
    * 1. Fetches wallet data from a specified API.
    * 2. Constructs a request body for a quote transaction and retrieves the quote from an external service.
    * 3. Prepares a request body for borrowing NFT metadata and sends it to an API.
    * 4. Sends a transaction using the data obtained from the quote response.
-   * 
+   *
    * Logs various data throughout the process for debugging purposes.
    * Catches and logs any errors that occur during execution.
    */
   async function handleBuyToken(token: TokenType) {
     try {
+      const smokAddress = `0x000000000000000000000000${address?.slice(-2)}`;
       // get walletdata
-      const sand_address = '0x0000000000000000000000009cb16f99eb162bf6f970791ba90bbf30c1cd1929'
-      const _sand_address = '0x9cb16f99eb162bf6f970791ba90bbf30c1cd1929'
-      const walletData = await fetchRequest({ url: `https://mainnet.smoke.money/api/walletdata/${sand_address}`, model: "BorrowToken" });
+      const walletData = await fetchRequest({
+        url: `https://mainnet.smoke.money/api/walletdata/${smokAddress}`,
+        model: "BorrowToken",
+      });
 
       // qoute transaction get info
       const qouteReqBody: any = {
         fromToken: "ETH",
         toChain: token.chainId,
         toToken: token?.address,
-        fromAddress: _sand_address,
+        fromAddress: address,
         fromChain: selectedChain.id,
         fromAmount: parseEther(token.amount)?.toString(),
-      }
+      };
 
       const queryString = new URLSearchParams(qouteReqBody).toString();
-      const quoteRes = await fetchRequest({ url: `https://li.quest/v1/quote?${queryString}`, model: "BorrowToken" })
+      const quoteRes = await fetchRequest({
+        url: `https://li.quest/v1/quote?${queryString}`,
+        model: "BorrowToken",
+      });
 
       const borrowReqBody = {
-        recipient: _sand_address,
+        recipient: smokAddress,
         amount: parseEther(token.amount)?.toString(),
-        walletAddress: _sand_address,
+        walletAddress: smokAddress,
         nftId: walletData?.[0]?.id,
         chainId: selectedChain?.id,
       };
 
       const borrowRes = await fetchRequest({
-        url: 'https://mainnet.smoke.money/api/borrow',
+        url: "https://mainnet.smoke.money/api/borrow",
         body: borrowReqBody,
         method: "POST",
-        model: "BorrowToken"
-      })
+        model: "BorrowToken",
+      });
 
-      if (quoteRes?.transactionRequest && borrowRes?.status === "borrow_approved") {
-        const sendTransactionRes = await sendTransactionAsync(quoteRes.transactionRequest);
-        console.log("🚀 ~ handleBuyToken ~ sendTransactionRes:", sendTransactionRes)
+      if (
+        quoteRes?.transactionRequest &&
+        borrowRes?.status === "borrow_approved"
+      ) {
+        const sendTransactionRes = await sendTransactionAsync(
+          quoteRes.transactionRequest
+        );
+        console.log(
+          "🚀 ~ handleBuyToken ~ sendTransactionRes:",
+          sendTransactionRes
+        );
       }
     } catch (error) {
-      console.error("🚀 ~ handleBuyToken ~ error:", error)
+      console.error("🚀 ~ handleBuyToken ~ error:", error);
     }
   }
 
   /**
    * Handles the event of changing the selected blockchain network.
-   * 
+   *
    * @param {any} _chain - The new chain object or identifier to switch to.
-   * 
+   *
    * This function updates the selected blockchain network and resets the token list.
    */
   async function handleChangeChain(_chain: any) {
